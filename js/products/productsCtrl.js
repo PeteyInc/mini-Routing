bda3f986ff305960dcd0d200f6acf5939d680e4a
Jared M. Peterson <jared@peteyinc.com>
2016-05-30 12:46:32 -0600

    finished

--- js/products/productsCtrl.js
+++ js/products/productsCtrl.js
@@ -1,3 +1,10 @@
-angular.module('miniRouting').controller('productsCtrl', function($scope) {
+angular.module('miniRouting').controller('productsCtrl', function ($scope, $stateParams, productService) {
 
+var productData = $stateParams.id;
+
+  if ($stateParams.id === 'shoes') {
+    $scope.productData = productService.shoeData;
+  } else if ($stateParams.id === 'socks') {
+    $scope.productData = productService.sockData;
+  }
 });
